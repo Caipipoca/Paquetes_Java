660485fbb12c4c71dfa9108101d7d2a210c0438b
caipipoca <caipipoca@caipipoca-X553MA>
2019-11-29 14:37:59 +0100

    Ejercicios Paquetes en java.

--- Paquetes_Java/src/main/java/Paquetes.java
+++ Paquetes_Java/src/main/java/Paquetes.java
@@ -10,4 +10,13 @@
  */
 public class Paquetes {
     
+    public static void main(String[] args) {
+        
+        System.out.println("Trabajando con paquetes: ");
+    }
+    
+    
+    
+    
+    
 }
